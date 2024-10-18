478cfa8adaad934b32daf9874bd5fd3b5de2c3b3
Daniel Mori Masuda <moridanibr@gmail.com>
2024-10-17 22:55:54 -0300

    Commit inicial

---
+++ app_7.py
@@ -0,0 +1,97 @@
+# Imports
+import pandas            as pd
+import streamlit         as st
+import seaborn           as sns
+import matplotlib.pyplot as plt
+from PIL                 import Image
+from io                  import BytesIO
+
+# Esta chamada deve ser a primeira e única chamada de `st.set_page_config()`
+st.set_page_config(page_title='Telemarketing analysis', page_icon=':phone:', layout='wide')
+
+# Set no tema do seaborn para melhorar o visual dos plots
+custom_params = {"axes.spines.right": False, "axes.spines.top": False}
+sns.set_theme(style="ticks", rc=custom_params)
+
+
+# Função para ler os dados
+@st.cache(show_spinner= True, allow_output_mutation=True)
+def load_data(file_data):
+    try:
+        return pd.read_csv(file_data, sep=';')
+    except:
+        return pd.read_excel(file_data)
+
+# Função para filtrar baseado na multiseleção de categorias
+@st.cache(allow_output_mutation=True)
+def multiselect_filter(relatorio, col, selecionados):
+    if 'all' in selecionados:
+        return relatorio
+    else:
+        return relatorio[relatorio[col].isin(selecionados)].reset_index(drop=True)
+
+# Função para converter o df para csv
+@st.cache
+def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+# Função para converter o df para excel
+@st.cache
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+
+def main():
+    # Não precisa mais chamar `st.set_page_config()` aqui
+
+    st.write('# Telemarketing Analysis')
+    st.markdown("---")
+    
+    # Apresenta a imagem na barra lateral
+    image = Image.open("Bank-Branding.jpg")
+    st.sidebar.image(image)
+
+    # Botão para carregar arquivo na aplicação
+    st.sidebar.write("## Suba o arquivo")
+    data_file_1 = st.sidebar.file_uploader("Bank marketing data", type = ['csv','xlsx'])
+
+    # Verifica se há conteúdo carregado na aplicação
+    if (data_file_1 is not None):
+        bank_raw = load_data(data_file_1)
+        bank = bank_raw.copy()
+
+        st.write('## Antes dos filtros')
+        st.write(bank_raw.head())
+
+        with st.sidebar.form(key='my_form'):
+
+            # SELECIONA O TIPO DE GRÁFICO
+            graph_type = st.radio('Tipo de gráfico:', ('Barras', 'Pizza'))
+        
+            # IDADES
+            max_age = int(bank.age.max())
+            min_age = int(bank.age.min())
+            idades = st.slider(label='Idade', 
+                               min_value = min_age,
+                               max_value = max_age, 
+                               value = (min_age, max_age),
+                               step = 1)
+
+# Inicia a aplicação
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
