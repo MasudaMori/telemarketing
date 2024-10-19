f56bbd9753ce928fd21c1727de401279ac89ccea
Daniel Mori Masuda <91749591+MasudaMori@users.noreply.github.com>
2024-10-19 18:58:10 -0300

    Update app_7.py

--- app_7.py
+++ app_7.py
@@ -1,3 +1,51 @@
+# Imports
+import pandas            as pd
+import streamlit         as st
+import seaborn           as sns
+import matplotlib.pyplot as plt
+from PIL                 import Image
+from io                  import BytesIO
+
+# Esta chamada deve ser a primeira e única chamada de `st.set_page_config()`
+st.set_page_config(page_title='Telemarketing analysis', page_icon=':phone:', layout='wide')
+
+# Set no tema do seaborn para melhorar o visual dos plots
+custom_params = {"axes.spines.right": False, "axes.spines.top": False}
+sns.set_theme(style="ticks", rc=custom_params)
+
+
+# Função para ler os dados
+@st.cache_data(show_spinner=True)
+def load_data(file_data):
+    try:
+        return pd.read_csv(file_data, sep=';')
+    except:
+        return pd.read_excel(file_data)
+
+# Função para filtrar baseado na multiseleção de categorias
+@st.cache_data
+def multiselect_filter(relatorio, col, selecionados):
+    if 'all' in selecionados:
+        return relatorio
+    else:
+        return relatorio[relatorio[col].isin(selecionados)].reset_index(drop=True)
+
+# Função para converter o df para csv
+@st.cache_data
+def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+# Função para converter o df para excel
+@st.cache_data
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+
 def main():
     st.write('# Telemarketing Analysis')
     st.markdown("---")
@@ -31,18 +79,9 @@ def main():
                                max_value=max_age, 
                                value=(min_age, max_age),
                                step=1)
-            
-            # Botão de envio
-            submit_button = st.form_submit_button(label='Aplicar')
-
-        # Processa as seleções apenas se o botão de envio for pressionado
-        if submit_button:
-            st.write(f"Gráfico selecionado: {graph_type}")
-            st.write(f"Idades selecionadas: {idades}")
 
 # Inicia a aplicação
 if __name__ == '__main__':
     main()
-
 
 
